7656a030d54788dd42f68238feb3fae307f2145a
Hgaffa <hgaffa@gmail.com>
2025-09-22 20:59:36 +0100

    Add frontend to dist

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ var cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 // Define token for getting the body from a request
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+morgan.token("body", function (req, res) {
+    return JSON.stringify(req.body);
+});
 
-app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens.body(req,res)
-    ].join(' ')
-}));
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, "content-length"),
+            "-",
+            tokens["response-time"](req, res),
+            "ms",
+            tokens.body(req, res),
+        ].join(" ");
+    })
+);
 
 const getRandomId = (max) => {
     return Math.floor(Math.random() * max);
-}
+};
 
 let persons = [
     {
@@ -50,58 +57,60 @@ let persons = [
 ];
 
 app.get("/", (request, response) => {
-    response.send("<h1>Hello World</h1>")
-})
+    response.send("<h1>Hello World</h1>");
+});
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
-})
+    response.json(persons);
+});
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
+    const id = request.params.id;
+    const person = persons.find((person) => person.id === id);
 
-    response.json(person)
-})
+    response.json(person);
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    const id = request.params.id;
+    persons = persons.filter((person) => person.id !== id);
 
-    response.status(204).end()
-})
+    response.status(204).end();
+});
 
 app.post("/api/persons", (request, response) => {
-    const body = request.body
+    const body = request.body;
 
     if (!body.name) {
         return response.status(400).json({
-            error: 'name missing'
-        })
+            error: "name missing",
+        });
     } else if (!body.number) {
         return response.status(400).json({
-            error: 'number missing'
-        })
+            error: "number missing",
+        });
     } else {
-        const existingPerson = persons.find(person => person.name === body.name)
+        const existingPerson = persons.find(
+            (person) => person.name === body.name
+        );
         if (existingPerson) {
             return response.status(409).json({
-                error: 'name must be unique'
-            })
+                error: "name must be unique",
+            });
         }
     }
 
     const person = {
         id: getRandomId(1000),
         name: body.name,
-        number: body.number
-    }
+        number: body.number,
+    };
 
-    persons = persons.concat(person)
-    response.json(person)
-})
+    persons = persons.concat(person);
+    response.json(person);
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
